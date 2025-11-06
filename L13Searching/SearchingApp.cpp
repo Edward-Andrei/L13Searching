e48d4dea872098531a8915b2370d638dcc33cba3
Edward-Andrei <andreed6540_aehit@students.vizja.pl>
2025-11-06 15:08:59 +0100

    Update SearchingApp.cpp

--- L13Searching/SearchingApp.cpp
+++ L13Searching/SearchingApp.cpp
@@ -1,18 +1,13 @@
 #include "Searching.h"
 #include <iostream>
-using namespace std;
 int main() {
-	int aux, result;
-	array<int, 10> a = { 2, 5, 7, 3, 4, 2, 9, 12, 1, 5 };
-
-	cout << "Enter an integer number: ";
-	cin >> aux;
-
-	result = linearSearch(a, aux);
+	std::array<int, 10> a = { 2,5,6,6,8,12,25,25,25,27 };
+	int result;
+	result = binarySearchFirst(a, 25);
 	if (result == -1) {
-		cout << "Element not found" << endl;
+		std::cout << "Element not found at array" << std::endl;
 	}
 	else {
-		cout << "The number " << aux << " is in the position " << result << endl;
+		std::cout << "Element found at index: " << result << std::endl;
 	}
 }
